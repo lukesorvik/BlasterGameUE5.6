25198a82cc4c931fa7273a443f9cf7ca953f3784
Luke Sorvik <lukesorvik@yahoo.com>
2025-10-18 17:50:42 -0700

    38 camera and spring arm

--- Source/Blaster/Private/Character/BlasterCharacter.cpp
+++ Source/Blaster/Private/Character/BlasterCharacter.cpp
@@ -3,12 +3,25 @@
 
 #include "Character/BlasterCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 // Sets default values
 ABlasterCharacter::ABlasterCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Create a subobject of this class named CameraBoom with the type USpringArmComponent
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetMesh()); // Attach to the mesh
+	CameraBoom->TargetArmLength = 600.f; // The camera follows at this distance behind the character
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller rotations
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom
+	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+	
 }
 
 // Called when the game starts or when spawned
